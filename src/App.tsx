56ab434c16bfd3548b28b8d46d0873e50699fd4d
Roman Mashta <roman.mashta@gmail.com>
2023-10-08 20:07:26 +0300

    tft-espi-wasm library initial integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
+import {useRef, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import {useTftEspi} from 'tft-espi-wasm'
+
 function App() {
   const [count, setCount] = useState(0)
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  useTftEspi(canvasRef);
 
   return (
     <>
       <div>
+        <canvas id={'canvas'} ref={canvasRef}></canvas>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo"/>
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React logo"/>
         </a>
       </div>
       <h1>Vite + React</h1>
